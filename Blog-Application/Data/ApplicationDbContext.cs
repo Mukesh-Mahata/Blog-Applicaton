9b20c85424aa677dec593c0e4c794c9a15c4e26c
Mukesh Mahata <mukeshmahata84@gmail.com>
2026-03-12 11:21:16 +0545

    Blog-application set up

---
+++ Blog-Application/Data/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog_Application.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+    {
+    }
+}
