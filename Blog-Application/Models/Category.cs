0bc1f237a35c9d9dffab0fa42d2f118f423f3e27
Mukesh Mahata <mukeshmahata84@gmail.com>
2026-03-12 11:26:39 +0545

    Category Comment Post model add

---
+++ Blog-Application/Models/Category.cs
@@ -0,0 +1,20 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace Blog_Application.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "The Category is Required")]
+        [MaxLength(100, ErrorMessage = "Category name cannot exceed 200 characters")]
+        public string Name { get; set; }
+
+        public string? Description { get; set; }
+
+
+
+    }
+}
---
+++ Blog-Application/Models/Comment.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Blog_Application.Models
+{
+    public class Comment
+    {
+        [Key]
+        public int Id { get; set; }
+
+
+        [Required(ErrorMessage = "The UserName is Required")]
+        [MaxLength(100, ErrorMessage = "The title cannot exceed 100 characters")]
+        public int UserName { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime CommentDate { get; set; } = DateTime.Now;
+
+        [Required]
+        public string Content { get; set; }
+
+    }
+}
