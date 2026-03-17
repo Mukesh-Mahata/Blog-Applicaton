fe2b3dfdcb74f6be03bc68237f6abb50e989b0f9
Mukesh Mahata <mukeshmahata84@gmail.com>
2026-03-17 12:38:17 +0545

    post create form create

---
+++ Blog-Application/Controllers/PostController.cs
@@ -0,0 +1,30 @@
+﻿using Blog_Application.Data;
+using Blog_Application.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Blog_Application.Controllers
+{
+    public class PostController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        // Constructor to inject the ApplicationDbContext
+        public PostController(ApplicationDbContext context)
+        {
+         _context = context;   
+        }
+        public IActionResult Create()
+        {
+            var postViewModel = new PostViewModel();
+            postViewModel.Categories = _context.Categories.Select(c =>
+                new SelectListItem
+                {
+                    Value = c.Id.ToString(),
+                    Text = c.Name
+                }
+            ).ToList();
+            return View(postViewModel);
+        }
+    }
+}
---
+++ Blog-Application/Models/ViewModels/PostViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Blog_Application.Models.ViewModels
+{
+    public class PostViewModel
+    {
+        public Post  Post{ get; set; }
+
+        public IEnumerable<SelectListItem> Categories { get; set; }
+
+        public IFormFile  FeatureImage{ get; set; }
+
+
+
+    }
+}
