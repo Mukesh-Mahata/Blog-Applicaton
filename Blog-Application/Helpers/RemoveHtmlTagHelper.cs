621fa379f71297d342477fc40f22fcc9d23e6511
Mukesh Mahata <mukeshmahata84@gmail.com>
2026-03-22 12:04:10 +0545

    add short content of index page

---
+++ Blog-Application/Helpers/RemoveHtmlTagHelper.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.RegularExpressions;
+using System.Net;
+
+namespace Blog_Application.Helpers
+{
+    public static class RemoveHtmlTagHelper
+    {
+        public static string RemoveHtmlTags(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+                return string.Empty;
+
+            // Remove HTML tags (non-greedy) then decode HTML entities
+            var withoutTags = Regex.Replace(input, "<.*?>", string.Empty);
+            return WebUtility.HtmlDecode(withoutTags);
+        }
+    }
+}
